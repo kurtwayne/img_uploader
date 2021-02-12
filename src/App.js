9bc1fab03dcfd486ecc8ea8c59aabf59fb6c491e
Kurt Wayne Gardner <kurtwgardner@Kurts-Air.lan>
2021-02-11 21:11:35 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+// import axios from 'axios';
 
-function App() {
+const { useState } = React;
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        The count is { counter }
+      </p>
+      <button onClick={() => {
+        setCounter(counter + 1)}
+      }>
+        +
+      </button>
+      <button onClick={() => {
+        setCounter(counter - 1)}
+        }>
+        -
+      </button>
     </div>
   );
 }
-
-export default App;
